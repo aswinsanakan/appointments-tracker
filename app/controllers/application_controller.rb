43eb123c370b46b7805fee9bf3e8041314260395
aswinsanakan <aswinsanakan@gmail.com>
2017-10-10 12:35:31 +0530

    Add google verification file for ownership

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def verify_google_crawler
+    render html: 'google-site-verification: google7e2388d998536b22.html'.html_safe
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   resources :appointments
 
+  get "/google7e2388d998536b22" => "application#verify_google_crawler"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
